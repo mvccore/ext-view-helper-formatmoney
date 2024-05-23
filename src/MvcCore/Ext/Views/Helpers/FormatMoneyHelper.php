baa5a3d53d66d6e7b0ec7a7c2384ac3e8c9e3ee8
Tom Flídr <tomflidr@gmail.com>
2024-05-23 09:08:58 +0200

    release v5.2.0

--- src/MvcCore/Ext/Views/Helpers/FormatMoneyHelper.php
+++ src/MvcCore/Ext/Views/Helpers/FormatMoneyHelper.php
@@ -39,7 +39,7 @@ class FormatMoneyHelper extends \MvcCore\Ext\Views\Helpers\FormatNumberHelper {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.0.1';
+	const VERSION = '5.2.0';
 
 	/**
 	 * If this static property is set - helper is possible
